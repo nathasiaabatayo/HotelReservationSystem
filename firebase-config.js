4c60a09821ee50850fca528db64d7c4c8e8a6524
Julia <juliabhombot093@gmail.com>
2025-06-14 04:13:12 +0800

    firebase-config

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyApL0S6df3dZ-xczcC0kuC-q-PVxEykIXo",
+  authDomain: "hotel-reservation-8b079.firebaseapp.com",
+  projectId: "hotel-reservation-8b079",
+  storageBucket: "hotel-reservation-8b079.appspot.com",
+  messagingSenderId: "414551921628",
+  appId: "1:414551921628:web:6daf3e402526874758b519"
+};
